aceffbe668c18a048dc8f3315f755ef190c6785d
dennoko <sd514dennoko@gmail.com>
2024-03-04 00:29:02 +0900

    XmlPullParser の依存関係を追加。 デシリアライズ後のデータを表す ApiBookData と、そのリストである ApiBookDataList データクラスを作成。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // XmlPullParser
+    implementation("xmlpull:xmlpull:1.1.3.4d_b4_min")
 }
---
+++ app/src/main/java/com/example/booksharingv2/xml_comverter/ApiBookDataList.kt
@@ -0,0 +1,5 @@
+package com.example.booksharingv2.xml_comverter
+
+data class ApiBookDataList(
+    val bookDataList: List<ApiBookData>
+)
