80699544f38279bc8b07f0569efbab81baeca69c
RUSSi28 <110999990+RUSSi28@users.noreply.github.com>
2024-03-26 16:07:43 +0900

    feat: UIのPreviewを作成しました

--- app/src/main/java/com/example/booksharingv2/view/screen/HomeScreen.kt
+++ app/src/main/java/com/example/booksharingv2/view/screen/HomeScreen.kt
@@ -2,8 +2,19 @@ package com.example.booksharingv2.view.screen
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.booksharingv2.ui.theme.BookSharingV2Theme
 
 @Composable
 fun HomeScreen() {
     Text(text = ("Home Screen"))
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeScreenPreview() {
+    BookSharingV2Theme {
+        HomeScreen()
+    }
 }
--- app/src/main/java/com/example/booksharingv2/view/screen/MyPage.kt
+++ app/src/main/java/com/example/booksharingv2/view/screen/MyPage.kt
@@ -2,8 +2,18 @@ package com.example.booksharingv2.view.screen
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.booksharingv2.ui.theme.BookSharingV2Theme
 
 @Composable
 fun MyPage() {
     Text(text = ("My Page"))
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MyPagePreview() {
+    BookSharingV2Theme {
+        MyPage()
+    }
 }
